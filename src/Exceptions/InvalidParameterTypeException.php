385351abaa38f23fd7b79bfd564c764b9c3ebfce
Ugo Brocard <ugo.brocard.official+github@gmail.com>
2023-10-23 13:47:46 +0200

    :sparkles: update: add parameter typing
    `@[name: int]` takes only `int values`
    
    Supported types are :
     - int
     - float
     - bool
     - str
    
    All other unsupported types returns strings

---
+++ src/Exceptions/InvalidParameterTypeException.php
@@ -0,0 +1,13 @@
+<?php 
+declare(strict_types = 1);
+
+namespace Router\Exceptions;
+
+use Exception;
+
+/**
+ * Class InvalidParameterTypeException
+ * 
+ * @package Router\Exceptions
+ */
+final class InvalidParameterTypeException extends Exception {}
--- src/Parameter.php
+++ src/Parameter.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 
 namespace Router;
 
+use Router\Exceptions\InvalidParameterTypeException;
+
 /**
  * Class Parameter
  * 
@@ -10,8 +12,11 @@ namespace Router;
  */
 final class Parameter
 {
-    const REGEX_PARAMETER      = "/^\@\[.+?\]$/";
-    const REGEX_PARAMETER_NAME = "/(?<=\@\[).+?(?=\])/";
+    const REGEX_PARAMETER       = "/^\@\[.+?\]$/";
+    const REGEX_PARAMETER_PARTS = "/(?<=\@\[)(\w*)(?:(?:\:(?: +)?)(\w*))?(?=\])/";
+
+    const REGEX_INT   = "/^\d*$/";
+    const REGEX_FLOAT = "/^\d*([\.|\,]\d*)?$/";
 
     /**
      * Method resolveParameters
@@ -38,9 +43,14 @@ final class Parameter
                 return null;
             }
 
-            preg_match(self::REGEX_PARAMETER_NAME, $pattern, $names);
-            $name              = $names[0];
-            $parameters[$name] = $input;
+            preg_match(self::REGEX_PARAMETER_PARTS, $pattern, $matches);
+
+            $name = $matches[1];
+            $type = $matches[2] ?? null;
+
+            $value = self::extractParameterValue($input, $type);
+
+            $parameters[$name] = $value;
         }
 
         return $parameters;
@@ -55,5 +65,33 @@ final class Parameter
     public static function isParameter(string $input): bool
     {
         return preg_match(self::REGEX_PARAMETER, $input) === 1;
+    }
+
+    protected static function extractParameterValue(string $input, ?string $type): string|int|float|bool|null {
+        $value = $input;
+        $type  = $type ? strtolower($type) : $type;
+
+        if ($type === "int") {
+            preg_match(self::REGEX_INT, $value, $matches);
+            $value = (int) $matches[0] ?? null;
+        }
+
+        if ($type === "float") {
+            preg_match(self::REGEX_FLOAT, $value, $matches);
+            $value = (float) str_replace(",", ".", $matches[0]) ?? null;
+        }
+
+        if ($type === "bool") {
+            if ($value === "true")  return true;
+            if ($value === "false") return false;
+
+            $value = null;
+        }
+
+        if ($value === null) {
+            throw new InvalidParameterTypeException;
+        }
+        
+        return $value;
     }
 }
